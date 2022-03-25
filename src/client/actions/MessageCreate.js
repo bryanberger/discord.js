dadd0fc70b9a9cec2d3d6c82a0933056e0884de9
Bryan Berger <bryan@bryanberger.com>
2022-03-25 18:17:44 -0400

    try/catch maybe?

--- src/client/actions/MessageCreate.js
+++ src/client/actions/MessageCreate.js
@@ -5,33 +5,40 @@ const { Events } = require('../../util/Constants');
 
 class MessageCreateAction extends Action {
   handle(data) {
-    const client = this.client;
-    const channel = this.getChannel(data);
-    if (channel) {
-      const existing = channel.messages.cache.get(data.id);
-      if (existing) return { message: existing };
-      const message = channel.messages.add(data);
-      const user = message.author;
-      let member = message.member;
-      channel.lastMessageID = data.id;
-      if (user) {
-        user.lastMessageID = data.id;
-        user.lastMessageChannelID = channel.id;
-      }
-      if (member) {
-        member.lastMessageID = data.id;
-        member.lastMessageChannelID = channel.id;
-      }
-
-      /**
-       * Emitted whenever a message is created.
-       * @event Client#message
-       * @param {Message} message The created message
-       */
-      client.emit(Events.MESSAGE_CREATE, message);
-      return { message };
+    try {
+      const client = this.client;
+      const channel = this.getChannel(data);
+      
+      if (channel) {
+        if (!channel.isText()) return {};
+  
+        const existing = channel.messages.cache.get(data.id);
+        if (existing) return { message: existing };
+        const message = channel.messages.add(data);
+        const user = message.author;
+        let member = message.member;
+        channel.lastMessageID = data.id;
+        if (user) {
+          user.lastMessageID = data.id;
+          user.lastMessageChannelID = channel.id;
+        }
+        if (member) {
+          member.lastMessageID = data.id;
+          member.lastMessageChannelID = channel.id;
+        }
+  
+        /**
+         * Emitted whenever a message is created.
+         * @event Client#message
+         * @param {Message} message The created message
+         */
+        client.emit(Events.MESSAGE_CREATE, message);
+        return { message };
+      }  
+    } catch(err) {
+      return {};
     }
-
+    
     return {};
   }
 }
--- src/client/actions/MessageDelete.js
+++ src/client/actions/MessageDelete.js
@@ -5,24 +5,28 @@ const { Events } = require('../../util/Constants');
 
 class MessageDeleteAction extends Action {
   handle(data) {
-    const client = this.client;
-    const channel = this.getChannel(data);
-    let message;
-    if (channel) {
-      message = this.getMessage(data, channel);
-      if (message) {
-        channel.messages.cache.delete(message.id);
-        message.deleted = true;
-        /**
-         * Emitted whenever a message is deleted.
-         * @event Client#messageDelete
-         * @param {Message} message The deleted message
-         */
-        client.emit(Events.MESSAGE_DELETE, message);
+    try {
+      const client = this.client;
+      const channel = this.getChannel(data);
+      let message;
+      if (channel) {
+        message = this.getMessage(data, channel);
+        if (message) {
+          channel.messages.cache.delete(message.id);
+          message.deleted = true;
+          /**
+           * Emitted whenever a message is deleted.
+           * @event Client#messageDelete
+           * @param {Message} message The deleted message
+           */
+          client.emit(Events.MESSAGE_DELETE, message);
+        }
       }
+  
+      return { message };
+    } catch(err) {
+      return {};
     }
-
-    return { message };
   }
 }
 
